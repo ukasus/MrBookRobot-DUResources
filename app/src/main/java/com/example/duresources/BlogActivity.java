d9f628e9fe03a00b2b63b52315b00b2db9b800ea
Swayam Gupta <guptaswayam075@gmail.com>
2020-08-09 02:20:20 +0530

    added functionality to dropdown and empty pages

---
+++ app/src/main/java/com/example/duresources/BlogActivity.java
@@ -0,0 +1,14 @@
+package com.example.duresources;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BlogActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blog);
+    }
+}
--- app/src/main/java/com/example/duresources/HomeActivity.java
+++ app/src/main/java/com/example/duresources/HomeActivity.java
@@ -5,14 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     TextView logout;
-
+    Button go;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +27,7 @@ public class HomeActivity extends AppCompatActivity {
                 );
         myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         mySpinner.setAdapter(myAdapter);
+        mySpinner.setOnItemSelectedListener(this);
 
         logout=findViewById(R.id.logout);
         logout.setOnClickListener(new View.OnClickListener() {
@@ -34,5 +37,42 @@ public class HomeActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        final String text=adapterView.getItemAtPosition(i).toString();
+        go=findViewById(R.id.button3);
+        go.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(text.equals("Blogs")){
+                    Intent i = new Intent(HomeActivity.this,BlogActivity.class);
+                    startActivity(i);
+                }
+                if(text.equals("Notes")){
+                    Intent i = new Intent(HomeActivity.this,NotesActivity.class);
+                    startActivity(i);
+                }
+                if(text.equals("Suggested Reading Material")){
+                    Intent i = new Intent(HomeActivity.this,SuggestedReadingsActivity.class);
+                    startActivity(i);
+                }
+                if(text.equals("Book Robot")){
+                    Intent i = new Intent(HomeActivity.this,BookRobotActivity.class);
+                    startActivity(i);
+                }
+                if(text.equals("GD PI Section")){
+                    Intent i = new Intent(HomeActivity.this,GDPIActivity.class);
+                    startActivity(i);
+                }
+
+            }
+        });
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
